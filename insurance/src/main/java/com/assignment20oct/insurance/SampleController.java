c9c88c59111117ea24e8cf5bd306bcc9de5e9a67
shivam-rai1234 <shivam.rai@kpipartners.com>
2022-10-21 17:14:53 +0530

    commit

--- insurance/src/main/java/com/assignment20oct/insurance/SampleController.java
+++ insurance/src/main/java/com/assignment20oct/insurance/SampleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,26 +22,61 @@ public class SampleController {
 	@Autowired
 	private ClientRepo c5;
 	
-	@GetMapping("/abhitayu_sharma")
+	@GetMapping("/abhitayu sharma")
 	public List<ClientEntity> getjoi(){
 		return c1.getjoin(1);
+	
 	}
-	@GetMapping("/akshit_anand")
-	public List<ClientEntity> getjo(){
+	@GetMapping("/akshit anand")
+	public List<ClientEntity> getjoi2(){
 		return c2.getjoin(2);
+	
 	}
-	@GetMapping("/shivam_rai")
-	public List<ClientEntity> getjoy(){
-		return c3.getjoin(3);
+	@GetMapping("/shivam rai")
+	public List<ClientEntity> getjoi3(){
+		return c3.getjoin(2);
+	
 	}
 	@GetMapping("/arghya")
-	public List<ClientEntity> getji(){
-		return c4.getjoin(5);
-	}
-	@GetMapping("/chachi_420")
-	public List<ClientEntity> getoi(){
-		return c5.getjoin(5);
-	}
+	public List<ClientEntity> getjoi4(){
+		return c4.getjoin(2);
 	
+	}
+	@GetMapping("/chachi 420")
+	public List<ClientEntity> getjoi5(){
+		return c5.getjoin(2);
+	
+	}
+	@GetMapping("/client_discount/{ten}/{premium}")
+	public int discount(@PathVariable String ten, @PathVariable int premium) {
+			
+			final String type1="greaterthan5";
+			final String type2="equalthan5";
+			final String type3="lessthan5";
+			
+			final int percentage=12;
+			final int percentag=10;
+			int a=0;
+		
+					if (ten==type1)
+					   {
+							
+							a=(premium*percentage)/100;
+							premium=premium-a;
+							return premium;
+					   }
+					if (ten.equals(type2))
+					   {
+							a=(premium*percentag)/100;
+							premium=premium-a;
+							return premium;
+					   }
+					else if (ten.equals(type3)) 
+						{
+							return premium;
+						}
+			return 0;
+		
+	}
 
 }
