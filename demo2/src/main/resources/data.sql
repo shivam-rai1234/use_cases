d360d0ba5fcc323f3a9cf6cdd0afcb27dcb1c1d0
shivam-rai1234 <shivam.rai@kpipartners.com>
2022-10-27 18:10:28 +0530

    COMMMIT FOR H2 DATA RDBMS

--- demo2/src/main/resources/data.sql
+++ demo2/src/main/resources/data.sql
@@ -1,6 +1,29 @@
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
-values(10001,'USD', 'INR' ,65,0);  
+values(1,'USD', 'RUPEE' ,82.43,01);  
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
-values(10002,'EUR', 'INR' ,75,0);  
+values(2,'USD', 'USD' ,1,02); 
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
-values(10003,'AUD', 'INR' ,25,0);  
+values(3,'USD', 'EURO' ,1,03); 
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(4,'USD', 'POUND' ,0.86,04); 
+
+
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(5,'EURO', 'RUPEE' ,75,0505);  
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(6,'EURO', 'EUR' ,1,0990);  
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(7,'EURO', 'USD' ,1,0998);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(8,'EURO', 'POUND' ,0.87,0987);
+
+
+
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(9,'RUPEE', 'RUPEE' ,1,9078);  
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(10,'RUPEE', 'EUR' ,0.012,8970);  
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(11,'RUPEE', 'USD' ,0.012,9090);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)  
+values(12,'RUPEE', 'POUND' ,0.010,9091);
